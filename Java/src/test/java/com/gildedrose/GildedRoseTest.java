877bc6e16a1f1cd55c65b5faf2c17e7deb446205
jvdbergh <jvandenberghe@isabelgroup.eu>
2020-03-31 10:30:16 +0200

    refactor: adding extra tests with sellIn and Quality

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -20,6 +20,19 @@ class GildedRoseTest {
     }
 
     @Test
+    void updateQualityBasicItemWithSellInAndQuality() {
+        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("+5 Dexterity Vest", app.getItems()[0].name);
+        assertEquals(4, app.getItems()[0].sellIn);
+        assertEquals(4, app.getItems()[0].quality);
+        app.updateQuality();
+        assertEquals(3, app.getItems()[0].sellIn);
+        assertEquals(3, app.getItems()[0].quality);
+    }
+
+    @Test
     void updateQualityAgedBrieItem() {
         Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
         GildedRose app = new GildedRose(items);
@@ -33,6 +46,23 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(-3, app.getItems()[0].sellIn);
         assertEquals(6, app.getItems()[0].quality);
+    }
+
+
+    @Test
+    void updateQualityAgedBrieItemWithSellInAndQuality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.getItems()[0].name);
+        assertEquals(4, app.getItems()[0].sellIn);
+        assertEquals(6, app.getItems()[0].quality);
+        app.updateQuality();
+        assertEquals(3, app.getItems()[0].sellIn);
+        assertEquals(7, app.getItems()[0].quality);
+        app.updateQuality();
+        assertEquals(2, app.getItems()[0].sellIn);
+        assertEquals(8, app.getItems()[0].quality);
     }
 
     @Test
@@ -52,6 +82,22 @@ class GildedRoseTest {
     }
 
     @Test
+    void updateQualitySulfurasItemWithSellInAndQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.getItems()[0].name);
+        assertEquals(5, app.getItems()[0].sellIn);
+        assertEquals(5, app.getItems()[0].quality);
+        app.updateQuality();
+        assertEquals(5, app.getItems()[0].sellIn);
+        assertEquals(5, app.getItems()[0].quality);
+        app.updateQuality();
+        assertEquals(5, app.getItems()[0].sellIn);
+        assertEquals(5, app.getItems()[0].quality);
+    }
+
+    @Test
     void updateQualityBackstageItem() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0) };
         GildedRose app = new GildedRose(items);
@@ -65,5 +111,21 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(-3, app.getItems()[0].sellIn);
         assertEquals(0, app.getItems()[0].quality);
+    }
+
+    @Test
+    void updateQualityBackstageItemWithSellInAndQuality() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.getItems()[0].name);
+        assertEquals(4, app.getItems()[0].sellIn);
+        assertEquals(8, app.getItems()[0].quality);
+        app.updateQuality();
+        assertEquals(3, app.getItems()[0].sellIn);
+        assertEquals(11, app.getItems()[0].quality);
+        app.updateQuality();
+        assertEquals(2, app.getItems()[0].sellIn);
+        assertEquals(14, app.getItems()[0].quality);
     }
 }
