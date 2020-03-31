0321b3778bf7567d3b8bc916e070beb1e0b78963
jvdbergh <jvandenberghe@isabelgroup.eu>
2020-03-31 16:55:39 +0200

    refactor: quality increase & decrease

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,6 +8,9 @@ class GildedRose {
 	public static final int MIN_QUALITY_TRESHOLD = 0;
 	public static final int MAX_QUALITY_TRESHOLD = 50;
 
+	public static final int NORMAL_DEGRADATION = 1;
+	public static final int FAST_DEGRADATION = 2;
+
 	private Item[] items;
 
 	public Item[] getItems() {
@@ -19,32 +22,34 @@ class GildedRose {
 	}
 
 	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			if (!items[i].name.equals(ITEM_AGED_BRIE)
-					&& !items[i].name.equals(ITEM_TAFKAL80ETC)
-					&& !items[i].name.equals(ITEM_SULFURAS)) {
-				handleBasicItem(items[i]);
+		for (Item item : items) {
+			if (!item.name.equals(ITEM_AGED_BRIE)
+					&& !item.name.equals(ITEM_TAFKAL80ETC)
+					&& !item.name.equals(ITEM_SULFURAS)) {
+				handleBasicItem(item);
 			}
 
-			if (items[i].name.equals(ITEM_TAFKAL80ETC)) {
-				handleBackStageItem(items[i]);
+			if (item.name.equals(ITEM_TAFKAL80ETC)) {
+				handleBackStageItem(item);
 			}
 
-			if (items[i].name.equals(ITEM_AGED_BRIE)) {
-				handleAgedBrie(items[i]);
+			if (item.name.equals(ITEM_AGED_BRIE)) {
+				handleAgedBrie(item);
 			}
 		}
 	}
 
 	private void handleBackStageItem(Item item) {
-		item.quality = incrementQuality(item.quality);
-		if (item.sellIn < 11) {
-			item.quality = incrementQuality(item.quality);
+		int qualityIncrease = 1;
+		if (item.sellIn < 6) {
+			qualityIncrease = 3;
 		}
 
-		if (item.sellIn < 6) {
-			item.quality = incrementQuality(item.quality);
+		if (item.sellIn >=  6 && item.sellIn < 11) {
+			qualityIncrease = 2;
 		}
+
+		item.quality = increaseQuality(item.quality, qualityIncrease);
 		item.sellIn = item.sellIn - 1;
 		if (item.sellIn < 0) {
 			item.quality = 0;
@@ -52,33 +57,31 @@ class GildedRose {
 	}
 
 	private void handleAgedBrie(Item item) {
-		item.quality = incrementQuality(item.quality);
+		item.quality = increaseQuality(item.quality, 1);
 		item.sellIn = item.sellIn - 1;
 		if (item.sellIn < 0) {
-			item.quality = incrementQuality(item.quality);
+			item.quality = increaseQuality(item.quality, 1);
 		}
 	}
 
-	private static int decreaseQuality(int quality) {
-		if (quality > MIN_QUALITY_TRESHOLD) {
-			quality = quality - 1;
+	private void handleBasicItem(Item item) {
+		item.quality = decreaseQuality(item.quality, (item.sellIn < 0? FAST_DEGRADATION: NORMAL_DEGRADATION));
+		item.sellIn = item.sellIn - 1;
+	}
+
+	private static int decreaseQuality(int quality, int degradation) {
+		quality = quality - degradation;
+		if (quality < MIN_QUALITY_TRESHOLD) {
+			quality = MIN_QUALITY_TRESHOLD;
 		}
 		return quality;
 	}
 
-	private static int incrementQuality(int currentQuality) {
-		if (currentQuality < GildedRose.MAX_QUALITY_TRESHOLD) {
-			currentQuality = currentQuality + 1;
+	private static int increaseQuality(int currentQuality, int increment) {
+		currentQuality = currentQuality + increment;
+		if (currentQuality > GildedRose.MAX_QUALITY_TRESHOLD) {
+			currentQuality = GildedRose.MAX_QUALITY_TRESHOLD;
 		}
 		return currentQuality;
-	}
-
-	private void handleBasicItem(Item item) {
-		int calculatedQuality = decreaseQuality(item.quality);
-		if (item.sellIn < 0){
-			calculatedQuality = decreaseQuality(calculatedQuality);
-		}
-		item.sellIn = item.sellIn - 1;
-		item.quality = calculatedQuality;
 	}
 }
